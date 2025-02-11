25a24ae5264e5f17cd9938545d01e39d4b2d422b
Luis Brayan <luis.bs.9004@gmail.com>
2025-02-11 01:08:10 -0600

    fix: parse buffer into string on script

--- scripts/update-manifest.mjs
+++ scripts/update-manifest.mjs
@@ -1,7 +1,7 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import { execSync } from 'node:child_process'
 
-const run = (command) => console.log(execSync(command))
+const run = (command) => console.log(execSync(command).toString('utf8'))
 
 const NEW_VERSION = process.env.npm_package_version
 
